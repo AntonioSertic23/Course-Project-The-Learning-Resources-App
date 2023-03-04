677f3abdf88d4c17ba3c4c7b608bed2c05f680c7
AntonioSertic23 <sertic43@gmail.com>
2023-03-04 08:41:22 +0100

    Dynamic Components & Attribute Fallthrough

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,38 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resources')"
+      >Add Resources</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"> </component>
+</template>
+
+<script>
+import BaseButton from '../../UI/BaseButton.vue';
+import BaseCard from '../../UI/BaseCard.vue';
+import StoredResources from './StoredResources.vue';
+import AddResources from './AddResources.vue';
+
+export default {
+  components: {
+    BaseCardBaseButton,
+    StoredResources,
+    AddResources,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
